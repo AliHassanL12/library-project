b54929302bfa8249e8d1d188df68a45d88268be2
Ali Hassan Liaquat <liaqqahl@gmail.com>
2024-01-04 04:59:29 +0000

    Add DOMClass and Display Book method

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
+
+
 class library {
     constructor() {
         this.myLibrary = [];
     }
-    
+
     addBookToLibrary() {
         const author = prompt("Who's the author?");
         const title = prompt("Book Title?");
@@ -10,7 +12,7 @@ class library {
         const hasRead = prompt("Have you read it already?");
 
         let aBook = new book(author, title, pages, hasRead);
-        bookLibrary.myLibrary.push(aBook);
+        this.myLibrary.push(aBook);
     }
 }
 
@@ -21,12 +23,25 @@ class book {
         this.pages = pages;
         this.reading = reading; 
     }
+}
 
+class DOMClass {
+    displayBooks() {
+        for (let i=0; i < bookLibrary.myLibrary.length; i++) {
+            const container = document.querySelector('.container');
+            const bookElement = document.createElement('div');
+            bookElement.classList.add('book');
+            bookElement.textContent = `${bookLibrary.myLibrary[i].title}\nhello this is`;
+            container.appendChild(bookElement);
+            console.log(bookLibrary.myLibrary[i].title);
+        }
+    }
 }
 let bookLibrary = new library();
-bookLibrary.addBookToLibrary();
+let dom = new DOMClass();
 let firstBook = new book('sa', 'sda', '32', 'yes');
 let secondBook = new book('la', '[ra', '32', 'no');
 bookLibrary.myLibrary.push(firstBook);
 bookLibrary.myLibrary.push(secondBook);
-console.log(bookLibrary.myLibrary)
+console.log(bookLibrary.myLibrary);
+dom.displayBooks();
