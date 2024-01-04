fb245c832c397f4e19c08d243f4735344f5d1610
Ali Hassan Liaquat <liaqqahl@gmail.com>
2024-01-04 01:45:46 +0000

    Place addBookToLibrary method to library class rather than book class

--- javascript.js
+++ javascript.js
@@ -2,6 +2,16 @@ class library {
     constructor() {
         this.myLibrary = [];
     }
+    
+    addBookToLibrary() {
+        const author = prompt("Who's the author?");
+        const title = prompt("Book Title?");
+        const pages = prompt("How many pages?");
+        const hasRead = prompt("Have you read it already?");
+
+        let aBook = new book(author, title, pages, hasRead);
+        bookLibrary.myLibrary.push(aBook);
+    }
 }
 
 class book {
@@ -12,18 +22,9 @@ class book {
         this.reading = reading; 
     }
 
-    addBookToLibrary() {
-        const author = prompt("Who's the author?");
-        const title = prompt("Book Title?");
-        const pages = prompt("How many pages?");
-        const hasRead = prompt("Have you read it already?");
-
-        let aBook = new book(author, title, pages, hasRead);
-        bookLibrary.myLibrary.push(aBook);
-    }
-
 }
 let bookLibrary = new library();
+bookLibrary.addBookToLibrary();
 let firstBook = new book('sa', 'sda', '32', 'yes');
 let secondBook = new book('la', '[ra', '32', 'no');
 bookLibrary.myLibrary.push(firstBook);
