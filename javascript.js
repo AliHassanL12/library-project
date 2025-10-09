dec989ff839c1cee68914bf9afab07727e6b9e4b
AliHassanL12 <liaqqahl@gmail.com>
2025-10-09 21:21:14 +0100

    change function constructors to class syntax

--- javascript.js
+++ javascript.js
@@ -1,27 +1,30 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title,
-    this.author = author, 
-    this.pages = pages,
-    this.read = read
-}
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-Book.prototype.generateID = function() {
-    this.id = crypto.randomUUID()
-}
+    generateID() {
+        this.id = crypto.randomUUID();
+    }
 
-Book.prototype.getID = function() {
-    return this.id;
-}
+    getID() {
+        return this.id;
+    }
 
-Book.prototype.changeRead = function() {
-    if (this.read === 'Yes') {
-        this.read = 'No';
-    } else {
-        this.read = 'Yes';
+    changeRead() {
+        if (this.read === 'Yes') {
+            this.read = 'No';
+        } else {
+            this.read = 'Yes';
+        }
     }
 }
+
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read)
     myLibrary.push(book);
